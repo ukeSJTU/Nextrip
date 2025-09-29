b867a5f7371dcb35c612a19e8abb99094f880c7f
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-09-30 07:28:05 +0800

    feat: add client side phone number plugin

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react";
+import { phoneNumberClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  plugins: [phoneNumberClient()],
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
