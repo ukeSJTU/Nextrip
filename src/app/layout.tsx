bcfeed18f7fbe9e708c023a2ec2d8a2609b37c3d
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-09-29 20:10:13 +0800

    feat: wrap layout in ThemeProvider and include Header and Footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/global/header";
+import Footer from "@/components/global/footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,8 +31,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
