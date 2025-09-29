662c8f89bdcc108a76a8abf46106bbd9c9e004b7
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-09-30 07:26:28 +0800

    feat: add phone number plugin to server

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
+import { phoneNumber } from "better-auth/plugins";
 
 export const auth = betterAuth({
   emailAndPassword: {
@@ -11,6 +12,11 @@ export const auth = betterAuth({
   }),
 
   plugins: [
-    // you can add plugins here
+    phoneNumber({
+      sendOTP: ({ phoneNumber, code }, request) => {
+        // TODO: Implement sending OTP code via SMS
+        console.log(`Sending OTP code ${code} to ${phoneNumber}`);
+      },
+    }),
   ],
 });
