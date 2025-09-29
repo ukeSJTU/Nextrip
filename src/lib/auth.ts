b989bf06592331cc3e709f548bafd88418dfd98b
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-09-29 23:46:35 +0800

    feat(auth): enable email and password auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,11 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db";
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    provider: "pg",
   }),
 
   plugins: [
