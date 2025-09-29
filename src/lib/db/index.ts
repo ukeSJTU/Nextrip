9d5a52a5ed66c1c600b708a8b7e59df52e8d5ba1
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-09-30 07:45:04 +0800

    fix: correct schema export and inport for drizzle adapter

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "@/lib/db/schema";
 
 export const db = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
+  schema,
 });
---
+++ src/lib/db/schema.ts
@@ -0,0 +1,2 @@
+// 重新导出根目录 auth-schema.ts 中的所有表结构
+export * from "../../../auth-schema";
